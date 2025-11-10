a358382bf7d2fecfb48ef2a0d2d4a1860d7ddf91
c0d9d <c0d9rs@gmail.com>
2025-11-10 11:11:28 +1000

    Delete lc directory

--- lc/lc/-Module-{FBB021E9-6A02-49F8-B3C9-C43EAD1F1591}.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-// Token: 0x02000013 RID: 19
-internal class <Module>{FBB021E9-6A02-49F8-B3C9-C43EAD1F1591}
-{
-}
--- lc/lc/VB$AnonymousDelegate_0.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-
-// Token: 0x02000002 RID: 2
-// (Invoke) Token: 0x06000004 RID: 4
-[System.Diagnostics.DebuggerDisplay("<generated method>", Type = "<generated method>")]
-[System.Runtime.CompilerServices.CompilerGenerated]
-internal delegate void VB$AnonymousDelegate_0();
